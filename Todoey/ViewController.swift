f6aefb1558a64cfec8d6647ec66a466b8ced2f40
olegp <o.pecherin@alpinadigital.ru>
2019-07-10 11:15:46 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by  oleg p on 10/07/2019.
+//  Copyright © 2019  oleg p. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewController: UITableViewController {
+    
+    let itemArray = ["oleg", "kira", "dima", "arina"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryType =
+            tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark ? .none : .checkmark
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
